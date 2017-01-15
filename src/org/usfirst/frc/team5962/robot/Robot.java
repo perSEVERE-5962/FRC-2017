6646d66cce9a497db6ac256c5cd8cfc6ccd77021
John <jkoopafreak@aol.com>
2017-01-15 14:52:32 -0500

    adding vision stuff from John

--- src/org/usfirst/frc/team5962/robot/Robot.java
+++ src/org/usfirst/frc/team5962/robot/Robot.java
@@ -35,6 +35,7 @@ public class Robot extends IterativeRobot {
         chooser.addDefault("Default Auto", new ExampleCommand());
 //        chooser.addObject("My Auto", new MyAutoCommand());
         SmartDashboard.putData("Auto mode", chooser);
+        RobotMap.init();
     }
 	
 	/**
--- src/org/usfirst/frc/team5962/robot/RobotMap.java
+++ src/org/usfirst/frc/team5962/robot/RobotMap.java
@@ -1,4 +1,9 @@
 package org.usfirst.frc.team5962.robot;
+
+import edu.wpi.first.wpilibj.RobotDrive;
+import edu.wpi.first.wpilibj.SpeedController;
+import edu.wpi.first.wpilibj.Victor;
+
 /**
  * The RobotMap is a mapping from the ports sensors and actuators are wired into
  * to a variable name. This provides flexibility changing wiring, makes checking
@@ -6,13 +11,47 @@ package org.usfirst.frc.team5962.robot;
  * floating around.
  */
 public class RobotMap {
-    // For example to map the left and right motors, you could define the
-    // following variables to use with your drivetrain subsystem.
-    // public static int leftMotor = 1;
-    // public static int rightMotor = 2;
     
-    // If you are using multiple modules, make sure to define both the port
-    // number and the module. For example you with a rangefinder:
-    // public static int rangefinderPort = 1;
-    // public static int rangefinderModule = 1;
+	//Victors
+//	private static Victor leftVictor1;
+//	private static Victor leftVictor2;
+//	private static Victor rightVictor1;
+//	private static Victor rightVictor2;
+	
+	//PWM Channels
+//	public final static int PWM_CHANNEL_0 = 0;
+//	public final static int PWM_CHANNEL_1 = 1;
+//	public final static int PWM_CHANNEL_2 = 2;
+//	public final static int PWM_CHANNEL_3 = 3;
+//	public final static int PWM_CHANNEL_4 = 4;
+//	public final static int PWM_CHANNEL_5 = 5;
+//	public final static int PWM_CHANNEL_6 = 6;
+//	public final static int PWM_CHANNEL_7 = 7;
+//	public final static int PWM_CHANNEL_8 = 8;
+//	public final static int PWM_CHANNEL_9 = 9;
+	
+	//DIO Channels
+//	public final static int DIO_CHANNEL_0 = 0;
+//	public final static int DIO_CHANNEL_1 = 1;
+//	public final static int DIO_CHANNEL_2 = 2;
+//	public final static int DIO_CHANNEL_3 = 3;
+//	public final static int DIO_CHANNEL_4 = 4;
+//	public final static int DIO_CHANNEL_5 = 5;
+//	public final static int DIO_CHANNEL_6 = 6;
+//	public final static int DIO_CHANNEL_7 = 7;
+//	public final static int DIO_CHANNEL_8 = 8;
+//	public final static int DIO_CHANNEL_9 = 9;
+	
+	public static void init(){
+		configureRobotDrive();
+	}
+	
+	private static void configureRobotDrive(){
+//		leftVictor1 = new Victor(PWM_CHANNEL_0);
+//		leftVictor2 = new Victor(PWM_CHANNEL_1);
+//		rightVictor1 = new Victor(PWM_CHANNEL_2);
+//		rightVictor2 = new Victor(PWM_CHANNEL_3);
+		
+	}
+	
 }
