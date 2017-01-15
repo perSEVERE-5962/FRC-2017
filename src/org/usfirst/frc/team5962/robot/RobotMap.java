1e35aa049ce70c82bd1516205ebab9ee143966e2
David <dlemasurier@comcast.net>
2017-01-15 14:32:16 -0500

    starting to add vision

--- src/org/usfirst/frc/team5962/robot/RobotMap.java
+++ src/org/usfirst/frc/team5962/robot/RobotMap.java
@@ -1,4 +1,9 @@
 package org.usfirst.frc.team5962.robot;
+
+import edu.wpi.first.wpilibj.RobotDrive;
+import edu.wpi.first.wpilibj.SpeedController;
+import edu.wpi.first.wpilibj.Victor;
+
 /**
  * The RobotMap is a mapping from the ports sensors and actuators are wired into
  * to a variable name. This provides flexibility changing wiring, makes checking
@@ -15,4 +20,27 @@ public class RobotMap {
     // number and the module. For example you with a rangefinder:
     // public static int rangefinderPort = 1;
     // public static int rangefinderModule = 1;
+	public static RobotDrive myRobot;
+	
+	public static Victor ledVictor;
+	public static Victor testRobotLeftVictor1;
+	public static Victor testRobotLeftVictor2;
+	public static Victor testRobotRightVictor1;
+	public static Victor testRobotRightVictor2;
+	
+	public static void init() {
+		ledVictor = new Victor(4);
+
+		SpeedController leftDrive;
+		SpeedController rightDrive;
+		testRobotLeftVictor1 = new Victor(0);
+		testRobotLeftVictor2 = new Victor(1);
+		testRobotRightVictor1 = new Victor(6);
+		testRobotRightVictor2 = new Victor(7);
+		
+	    leftDrive = new MultiSpeedController(testRobotLeftVictor1, testRobotLeftVictor2);
+	    rightDrive = new MultiSpeedController(testRobotRightVictor1, testRobotRightVictor2);
+		myRobot = new RobotDrive(leftDrive, rightDrive);
+
+	}
 }
