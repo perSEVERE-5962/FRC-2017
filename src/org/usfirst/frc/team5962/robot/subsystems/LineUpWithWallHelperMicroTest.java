efb3d0f0cbb4fb21f4dc6d1ab416a735dd0a5f5e
Raj Patel <rajpatel99252@gmail.com>
2017-03-02 18:47:31 -0500

    adding code and tests for using two ultrasonics to line the robot up with the wall

--- src/org/usfirst/frc/team5962/robot/subsystems/LineUpWithWallHelperMicroTest.java
+++ src/org/usfirst/frc/team5962/robot/subsystems/LineUpWithWallHelperMicroTest.java
@@ -3,23 +3,20 @@ package org.usfirst.frc.team5962.robot.subsystems;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
-import org.usfirst.frc.team5962.robot.RobotMap;
 import org.usfirst.frc.team5962.robot.sensors.RobotUltrasonicBase;
 
 import junit.framework.TestCase;
 
-public class LineUpWithWallHelperMicroTest  extends TestCase{
-	
+public class LineUpWithWallHelperMicroTest extends TestCase {
+
 	/*
-	 * NOTE: Install EclEmma for code coverage
-	 * URL = http://update.eclemma.org/
+	 * NOTE: Install EclEmma for code coverage URL = http://update.eclemma.org/
 	 */
-	
+
 	private MockRobotUltrasonicAnalog ultrasonicLeft = new MockRobotUltrasonicAnalog();
 	private MockRobotUltrasonicAnalog ultrasonicRight = new MockRobotUltrasonicAnalog();
-	
+
 	private LineUpWithWallHelper lineUpWithWallHelper = new MockLineUpWithWallHelper();
-	
 
 	@Before
 	public void setUp() throws Exception {
@@ -36,27 +33,27 @@ public class LineUpWithWallHelperMicroTest  extends TestCase{
 		ultrasonicLeft.rangeInInches = 9.0;
 		ultrasonicRight.rangeInInches = 8.5;
 		lineUpWithWallHelper.lineUp(ultrasonicLeft, ultrasonicRight);
-		assertEquals(-0.6, ((MockLineUpWithWallHelper)lineUpWithWallHelper).outputMagnitude);
-		assertEquals(0.0, ((MockLineUpWithWallHelper)lineUpWithWallHelper).curve);
+		assertEquals(-0.6, ((MockLineUpWithWallHelper) lineUpWithWallHelper).outputMagnitude);
+		assertEquals(0.0, ((MockLineUpWithWallHelper) lineUpWithWallHelper).curve);
 
 		ultrasonicLeft.rangeInInches = 9.0;
 		ultrasonicRight.rangeInInches = 7.5;
 		lineUpWithWallHelper.lineUp(ultrasonicLeft, ultrasonicRight);
-		assertEquals(-0.6, ((MockLineUpWithWallHelper)lineUpWithWallHelper).outputMagnitude);
-		assertEquals(0.0, ((MockLineUpWithWallHelper)lineUpWithWallHelper).curve);
+		assertEquals(-0.6, ((MockLineUpWithWallHelper) lineUpWithWallHelper).outputMagnitude);
+		assertEquals(0.0, ((MockLineUpWithWallHelper) lineUpWithWallHelper).curve);
 
 		// right is greater than left
 		ultrasonicLeft.rangeInInches = 8.5;
 		ultrasonicRight.rangeInInches = 9;
 		lineUpWithWallHelper.lineUp(ultrasonicLeft, ultrasonicRight);
-		assertEquals(-0.6, ((MockLineUpWithWallHelper)lineUpWithWallHelper).outputMagnitude);
-		assertEquals(0.0, ((MockLineUpWithWallHelper)lineUpWithWallHelper).curve);
-		
+		assertEquals(-0.6, ((MockLineUpWithWallHelper) lineUpWithWallHelper).outputMagnitude);
+		assertEquals(0.0, ((MockLineUpWithWallHelper) lineUpWithWallHelper).curve);
+
 		ultrasonicLeft.rangeInInches = 7.5;
 		ultrasonicRight.rangeInInches = 9.5;
 		lineUpWithWallHelper.lineUp(ultrasonicLeft, ultrasonicRight);
-		assertEquals(-0.6, ((MockLineUpWithWallHelper)lineUpWithWallHelper).outputMagnitude);
-		assertEquals(0.0, ((MockLineUpWithWallHelper)lineUpWithWallHelper).curve);
+		assertEquals(-0.6, ((MockLineUpWithWallHelper) lineUpWithWallHelper).outputMagnitude);
+		assertEquals(0.0, ((MockLineUpWithWallHelper) lineUpWithWallHelper).curve);
 
 	}
 
@@ -67,48 +64,50 @@ public class LineUpWithWallHelperMicroTest  extends TestCase{
 		ultrasonicLeft.rangeInInches = 7.0;
 		ultrasonicRight.rangeInInches = 6.5;
 		lineUpWithWallHelper.lineUp(ultrasonicLeft, ultrasonicRight);
-		assertEquals(-0.6, ((MockLineUpWithWallHelper)lineUpWithWallHelper).outputMagnitude);
-		assertEquals(0.0, ((MockLineUpWithWallHelper)lineUpWithWallHelper).curve);
-		
+		assertEquals(-0.6, ((MockLineUpWithWallHelper) lineUpWithWallHelper).outputMagnitude);
+		assertEquals(0.0, ((MockLineUpWithWallHelper) lineUpWithWallHelper).curve);
+
 		ultrasonicLeft.rangeInInches = 5.3;
 		ultrasonicRight.rangeInInches = 4.5;
 		lineUpWithWallHelper.lineUp(ultrasonicLeft, ultrasonicRight);
-		assertEquals(-0.6, ((MockLineUpWithWallHelper)lineUpWithWallHelper).outputMagnitude);
-		assertEquals(0.0, ((MockLineUpWithWallHelper)lineUpWithWallHelper).curve);
+		assertEquals(-0.6, ((MockLineUpWithWallHelper) lineUpWithWallHelper).outputMagnitude);
+		assertEquals(0.0, ((MockLineUpWithWallHelper) lineUpWithWallHelper).curve);
 		// right is greater than left
 		ultrasonicLeft.rangeInInches = 6.5;
 		ultrasonicRight.rangeInInches = 7;
 		lineUpWithWallHelper.lineUp(ultrasonicLeft, ultrasonicRight);
-		assertEquals(-0.6, ((MockLineUpWithWallHelper)lineUpWithWallHelper).outputMagnitude);
-		assertEquals(0.0, ((MockLineUpWithWallHelper)lineUpWithWallHelper).curve);
-		
+		assertEquals(-0.6, ((MockLineUpWithWallHelper) lineUpWithWallHelper).outputMagnitude);
+		assertEquals(0.0, ((MockLineUpWithWallHelper) lineUpWithWallHelper).curve);
+
 		ultrasonicLeft.rangeInInches = 4.5;
 		ultrasonicRight.rangeInInches = 5.3;
 		lineUpWithWallHelper.lineUp(ultrasonicLeft, ultrasonicRight);
-		assertEquals(-0.6, ((MockLineUpWithWallHelper)lineUpWithWallHelper).outputMagnitude);
-		assertEquals(0.0, ((MockLineUpWithWallHelper)lineUpWithWallHelper).curve);
+		assertEquals(-0.6, ((MockLineUpWithWallHelper) lineUpWithWallHelper).outputMagnitude);
+		assertEquals(0.0, ((MockLineUpWithWallHelper) lineUpWithWallHelper).curve);
 	}
-	
+
 	@Test
-	// robot in range (closer than 8 inches), but right is greater than left by more than one inch
+	// robot in range (closer than 8 inches), but right is greater than left by
+	// more than one inch
 	public void testRightIsGreaterThanLeftByMoreThan1() {
 		ultrasonicLeft.rangeInInches = 5.5;
 		ultrasonicRight.rangeInInches = 7.5;
 		lineUpWithWallHelper.lineUp(ultrasonicLeft, ultrasonicRight);
-		assertEquals(-0.25, ((MockLineUpWithWallHelper)lineUpWithWallHelper).outputMagnitude);
-		assertEquals(-1.0, ((MockLineUpWithWallHelper)lineUpWithWallHelper).curve);
+		assertEquals(-0.25, ((MockLineUpWithWallHelper) lineUpWithWallHelper).outputMagnitude);
+		assertEquals(-1.0, ((MockLineUpWithWallHelper) lineUpWithWallHelper).curve);
 	}
-	
+
 	@Test
-	// robot in range (closer than 8 inches), but left is greater than right by more than one inch
+	// robot in range (closer than 8 inches), but left is greater than right by
+	// more than one inch
 	public void testLeftIsGreaterThanRightByMoreThan1() {
 		ultrasonicLeft.rangeInInches = 7.5;
 		ultrasonicRight.rangeInInches = 5.5;
 		lineUpWithWallHelper.lineUp(ultrasonicLeft, ultrasonicRight);
-		assertEquals(-0.25, ((MockLineUpWithWallHelper)lineUpWithWallHelper).outputMagnitude);
-		assertEquals(1.0, ((MockLineUpWithWallHelper)lineUpWithWallHelper).curve);
+		assertEquals(-0.25, ((MockLineUpWithWallHelper) lineUpWithWallHelper).outputMagnitude);
+		assertEquals(1.0, ((MockLineUpWithWallHelper) lineUpWithWallHelper).curve);
 	}
-	
+
 	@Test
 	// stop the robot
 	public void testBothWithin5() {
@@ -116,28 +115,28 @@ public class LineUpWithWallHelperMicroTest  extends TestCase{
 		ultrasonicLeft.rangeInInches = 4.9;
 		ultrasonicRight.rangeInInches = 4.5;
 		lineUpWithWallHelper.lineUp(ultrasonicLeft, ultrasonicRight);
-		assertEquals(0.0, ((MockLineUpWithWallHelper)lineUpWithWallHelper).outputMagnitude);
-		assertEquals(0.0, ((MockLineUpWithWallHelper)lineUpWithWallHelper).curve);
+		assertEquals(0.0, ((MockLineUpWithWallHelper) lineUpWithWallHelper).outputMagnitude);
+		assertEquals(0.0, ((MockLineUpWithWallHelper) lineUpWithWallHelper).curve);
 		// test left ahead of right
 		ultrasonicLeft.rangeInInches = 4.5;
 		ultrasonicRight.rangeInInches = 4.9;
 		lineUpWithWallHelper.lineUp(ultrasonicLeft, ultrasonicRight);
-		assertEquals(0.0, ((MockLineUpWithWallHelper)lineUpWithWallHelper).outputMagnitude);
-		assertEquals(0.0, ((MockLineUpWithWallHelper)lineUpWithWallHelper).curve);
+		assertEquals(0.0, ((MockLineUpWithWallHelper) lineUpWithWallHelper).outputMagnitude);
+		assertEquals(0.0, ((MockLineUpWithWallHelper) lineUpWithWallHelper).curve);
 	}
-	
+
 	class MockRobotUltrasonicAnalog extends RobotUltrasonicBase {
 
 		protected double rangeInInches = 0;
-		
+
 		public MockRobotUltrasonicAnalog() {
 			super();
 			// TODO Auto-generated constructor stub
 		}
 
-	    public double getRange() {
-	    	return rangeInInches;
-	    }
+		public double getRange() {
+			return rangeInInches;
+		}
 
 		@Override
 		public boolean isEnabled() {
@@ -146,12 +145,12 @@ public class LineUpWithWallHelperMicroTest  extends TestCase{
 		}
 
 	}
-	
+
 	class MockLineUpWithWallHelper extends LineUpWithWallHelper {
-		
+
 		protected double outputMagnitude = 0;
 		protected double curve = 0;
-		
+
 		protected void driveRobot(double outputMagnitude, double curve) {
 			this.outputMagnitude = outputMagnitude;
 			this.curve = curve;
